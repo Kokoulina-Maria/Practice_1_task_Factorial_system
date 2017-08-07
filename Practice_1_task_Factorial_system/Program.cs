fb0d0fb7c2619dc0dfd33550a51b6c0a85ac8d31
Maria <Maria@LAPTOP-N156UH30>
2017-08-07 18:17:44 +0500

    Добавьте файлы проекта.

---
+++ Practice_1_task_Factorial_system/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_1_task_Factorial_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
