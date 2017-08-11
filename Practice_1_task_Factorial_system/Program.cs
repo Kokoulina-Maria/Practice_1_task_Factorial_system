ba37e1d3ce29fd6c97dea1d44e713fa27f248595
Maria <Maria@LAPTOP-N156UH30>
2017-08-11 22:12:32 +0500

    Задача протестирована ситемой на сайте, ошибок нетЮ но программа не работает, так как записана неверно.

--- Practice_1_task_Factorial_system/Program.cs
+++ Practice_1_task_Factorial_system/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+
 
 namespace Practice_1_task_Factorial_system
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            StreamReader sr = new StreamReader("INPUT.TXT");
+            StreamWriter sw = new StreamWriter("OUTPUT.TXT");
+            string[] s = sr.ReadLine().Split();
+            int p = int.Parse(s[0]);//числитель
+            int q = int.Parse(s[1]);//знаменатель
+            int n = 1;//количество значащих разрядов плюс 1, основание системы
+            int[] a = new int[1000];//массив, в котором будут храниться ai (разряды числа в факториальной системе)
+            while (p > 0)//выполняем перевод из десятичной системы в факториальную так же, как и в другую позиционную СИ, меняя только ее основание (2, 3, 4...)
+                //пока дробь (т.е. ее числитель) не станет равна 0
+            {
+                n = n + 1;//вычисляем новое основание
+                p = n * p;//умножаем дробь (числитель) на новое основание
+                a[n]= p / q;//ищем целую часть, записываем ее в массив
+                p = p % q;//избавляемся от целой части
+            }
+            sw.WriteLine(n);//выводим количество значащих разрядов плюс 1
+            for (int i = 2; i <= n; i++)
+            {
+                sw.WriteLine(a[i]);
+            }
+            sr.Close();
+            sw.Close();
         }
     }
 }
